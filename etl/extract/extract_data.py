9ca16b6ac39e024b68403c476788a87a7662a0a7
akotchaye <akotchaybatcho@gmail.com>
2025-05-06 12:28:43 +0200

    adding extract function

---
+++ etl/extract/extract_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv("C:/myproject/ircantec/data/raw/raw_ircantec.csv", sep=";")
+
+
+def extract():
+    # Read the dataset into memory
+    data = pd.read_csv("C:/myproject/ircantec/data/raw/raw_ircantec.csv", sep=";")
+
+    # Details about the file
+    print("Here is a little bit of information about the data stored in ", data)
+    print(
+        f"\nThere are {data.shape[0]} rows and {data.shape[1]} columns in this DataFrame."
+    )
+    print("\nThe columns in this DataFrame take the following types: ")
+
+    # Print the type of each column
+    print(data.dtypes)
+
+    # Print a message before returning the DataFrame
+    print(
+        "\nTo view the DataFrame extracted from 'raw directory', display the value returned by this function!\n\n"
+    )
+    return data
