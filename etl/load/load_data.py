027b2a10a7a90ce9ad23904f3002e8ff931e18d9
akotchaye <akotchaybatcho@gmail.com>
2025-05-07 21:36:43 +0200

    adding some stuff to load_data.py

--- etl/load/load_data.py
+++ etl/load/load_data.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
-#define the load function to load the transformed data into database
+# define the load function to load the transformed data into database
+
 
 def load(data, file_path):
-    data.to_csv(name=table_name,, index=False)
-    print("Original dataframe has been loaded to mysql\n")
+    data.to_csv(file_path, index=False)
+    # remplacer par  un logging.info
+    print("The transformed data has been loaded into the processed directory\n")
 
     # Read the data, and return the result
-    loaded_dataframe = pd.read_sql(f"SELECT * FROM {table_name}", db_engine)
+    loaded_dataframe = pd.read_csv(
+        "C:/myproject/ircantec/data/raw/raw_ircantec.csv", sep=";"
+    )
+
     print("The loaded DataFrame has been read from mysql for validation\n")
 
     try:
-        assert dataframe.shape == loaded_dataframe.shape
+        assert data.shape == loaded_dataframe.shape
         print(
-            f" Success! The data in the {table_name} table have successfully been loaded and validated"
+            "Success! The data in the table have successfully been loaded and validated"
         )
 
     except AssertionError:
@@ -21,4 +26,5 @@ def load(data, file_path):
             "DataFrame shape is not consistent before and after loading. Take a closer look!"
         )
 
-#corriger la fontion!!!!
+
+# tester  et corriger!!!!
