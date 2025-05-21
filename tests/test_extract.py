b65477dc8d2cdea222191baaf2bd4935b5589e7c
akotchaye <akotchaybatcho@gmail.com>
2025-05-21 13:31:54 +0200

    adding a  file_path to the extract test function

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -4,5 +4,5 @@
 def test_extract_returns_dataframe():
     from etl.extract import extract
 
-    df = extract()
+    df = extract("C:/myproject/ircantec/data/raw/raw_ircantec.csv")
     assert df is not None
